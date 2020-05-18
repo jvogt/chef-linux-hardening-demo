6929ad1804c61ff196d453069dd95bc7a7f2bc37
Jeff Vogt <jvogt@chef.io>
2020-05-17 21:55:57 -0700

    updates cleanup

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -1,15 +1,7 @@
 name 'base'
 
-run_list 'os-hardening::default' # , 'audit::default'
+run_list 'os-hardening::default'
 
 default_source :supermarket
 
 cookbook 'os-hardening', path: '../cookbooks/os-hardening'
-
-# default['audit']['reporter'] = 'chef-automate'
-# default['audit']['profiles'].push(
-#   {
-#     'name': 'cis-centos7-level1',
-#     'compliance': 'admin/cis-centos7-level1',
-#   }
-# )
